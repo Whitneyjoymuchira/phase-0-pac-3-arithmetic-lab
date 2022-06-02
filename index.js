bbf8bf417dc4708db8a66d5834d575e7ec4021d8
Whitney Muchira <whitneymuchira@gmail.com>
2022-06-02 22:54:06 +0300

    submission

---
+++ index.js
@@ -0,0 +1,37 @@
+function add(){
+
+};                    
+function subtract() {
+
+};
+function multiply (){
+
+};
+function divide () {
+
+};
+function add( a,b){
+    return a+b 
+};
+function subtract(c,d){
+    return c-d
+};
+function multiply(d,e){
+    return e*d
+};
+function divide(e,f){
+    return e/f
+};
+function increment(f){
+    return ++f
+};
+function decrement(h) {
+    return --h
+};
+function makeInt(n){
+    return parseInt(n,10)
+};
+
+function preserveDecimal(n){
+    return parseFloat(n,n)
+};
